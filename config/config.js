3f12694419075d369d4121dc0458c9be9d88cf94
drincerkini <dc53912@ubt-uni.net>
2023-10-03 23:58:20 +0200

    added global variables configuration

--- config/config.js
+++ config/config.js
@@ -1,3 +1,23 @@
+// module.exports = {
+//     JWT_SECRET: 'your_secret_key'
+// }
+
+
+require('dotenv').config();
+
+//dotenv global variables 
+
+const MONGO_CONNECTION_URI = process.env.MONGO_CONNECTION_URI;
+const MONGO_PORT = process.env.MONGO_PORT;
+const MONGO_DB_NAME = process.env.MONGO_DB_NAME;
+const API_PORT = process.env.API_PORT;
+
+const JWT_SECRET =  'your_secret_key'
+
 module.exports = {
-    JWT_SECRET: 'your_secret_key'
+    MONGO_CONNECTION_URI,
+    MONGO_PORT,
+    MONGO_DB_NAME,
+    API_PORT,
+    JWT_SECRET
 }
--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
 const authenticateToken = require('./middlewares/authMiddleware');
+const { MONGO_CONNECTION_URI, MONGO_PORT, MONGO_DB_NAME, API_PORT } = require('./config/config');
+
 
 const app = express();
-const PORT = 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/auth-system', {
+mongoose.connect(`${MONGO_CONNECTION_URI}:${MONGO_PORT}/${MONGO_DB_NAME}`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -18,40 +19,14 @@ mongoose.connection.once('open', () => {
 
 app.use(bodyParser.json());
 
-// User Registration
-// app.post('/register', async (req, res) => {
-//   try {
-//     const { username, password } = req.body;
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     const user = new User({ username, password: hashedPassword });
-//     await user.save();
-//     res.status(201).json({ message: 'User registered successfully' });
-//   } catch (error) {
-//     console.error(error);
-//     res.status(500).json({ message: 'Internal Server Error' });
-//   }
-// });
-
-// User Login
+//user auth path (register, login)
 app.use('/auth', authRoutes);
-
-// Middleware to authenticate requests
-// const authenticateToken = (req, res, next) => {
-//   const token = req.header('Authorization');
-//   if (!token) return res.status(401).json({ message: 'Access denied' });
-
-//   jwt.verify(token, JWT_SECRET, (err, user) => {
-//     if (err) return res.status(403).json({ message: 'Invalid token' });
-//     req.user = user;
-//     next();
-//   });
-// };
 
 // Example protected route
 app.get('/protected', authenticateToken, (req, res) => {
   res.json({ message: 'This is a protected route', user: req.user });
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(API_PORT, () => {
+  console.log(`Server is running on port ${API_PORT}`);
 });
